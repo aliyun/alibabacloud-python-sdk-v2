94ef9cc4f732f8f6769bf39bde337a5a34fb164b
wallisyan <15936132608@163.com>
2019-08-22 14:26:14 +0800

    modify version

--- alibabacloud/__init__.py
+++ alibabacloud/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # -*- coding: utf-8 -*-
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 import logging
 
