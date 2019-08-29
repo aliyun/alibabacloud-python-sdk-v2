4fa1c37dad28292736d612badfc4542d889be108
wallisyan <15936132608@163.com>
2019-08-29 17:45:19 +0800

    modify lint check

--- scripts/code-style-check.sh
+++ scripts/code-style-check.sh
@@ -1,3 +1,3 @@
-pycodestyle --statistics alibabacloud/  --max-line-length=100 --ignore=W391,E121,E123,E126,E226,E24,E704,W503,W504 --exclude=DescribeEndpointsRequest.py,vendored
+pycodestyle --statistics alibabacloud/  --max-line-length=100 --ignore=W391,E121,E123,E126,E226,E24,E704,W503,W504 --exclude=DescribeEndpointsRequest.py,vendored,clients,services
 pycodestyle --statistics tests/  --max-line-length=100 --ignore=W391,E121,E123,E126,E226,E24,E704,W503,W504
 
